3b65b70854f5ca0cce8959104d24aabc49ba4ea1
pedrocauan <mainyasuo92@gmail.com>
2022-09-07 02:00:32 -0300

    refactoring

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const axios = require("axios")
 const { restart } = require("nodemon")
 
 
-app.use(cors())
+app.use(cors()) //Evita problemas relacionados a conflitos de portas com o browser
 
 //Rota que tá a API
 app.get("/", async (req, res) => {
+    /*==== SERVINDO A API === */
     try {
         //response é a resposta do axios, mas eu tiro o data de dentro do response
         const { data } = await axios("https://jsonplaceholder.typicode.com/users") /*endereço da api que o backend vai consumir*/
